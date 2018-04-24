047e3f0a47af503dac30345671af8b1b90175c69
Irina Brennen <irinabrennen@twc.com>
2018-04-23 20:18:56 -0500

    passing tests 1 through 5

--- js/index.js
+++ js/index.js
@@ -6,6 +6,23 @@ const svg = d3.select("body")
            .attr("width", w)
            .attr("height", h);
 
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("class","cell")
+    .attr("width",10)
+    .attr("height",20)
+    .attr("x",10)
+    .attr("y",20)
+    .attr("rx",10)
+    .attr("ry",20);
 
 svg.append("g")
-   .attr("id","x-axis")
+    .attr("id","x-axis");
+
+svg.append("g")
+    .attr("id","y-axis");
+
+
+
