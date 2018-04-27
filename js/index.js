ee4b7d96b4c049800293d5cfd68878717cea2efa
Irina Brennen <irinabrennen@twc.com>
2018-04-26 21:34:10 -0500

    passing test 10 My heat map should have cells that align with the corresponding year on the x-axis

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 const w = 1500;
 const h = 750;
+const padding = 80;
 const dataset = originalDataset.monthlyVariance;
+const minYear = d3.min(dataset, d=>d.year);
+const maxYear = d3.max(dataset, d=>d.year);
+const xRange = maxYear - minYear;
+const rectWidth = 5 ;
+const rectHeight = 20 ;
+
+const xScale =  d3.scaleLinear()
+                  .domain([minYear - (xRange * .05), maxYear +  (xRange * .05)])
+                  .range([padding, w-padding]);
+
 
 
 const svg = d3.select("body")
@@ -13,9 +24,9 @@ svg.selectAll("rect")
     .enter()
     .append("rect")
     .attr("class","cell")
-    .attr("width",10)
-    .attr("height",20)
-    .attr("x",10)
+    .attr("width",rectWidth)
+    .attr("height",rectHeight)
+    .attr("x",d => xScale(d.year))
     .attr("y",20)
     .attr("rx",10)
     .attr("ry",20)
@@ -23,11 +34,16 @@ svg.selectAll("rect")
     .attr("data-year", d => d.year)
     .attr("data-temp", d => originalDataset.baseTemperature + d.variance);
 
-svg.append("g")
-    .attr("id","x-axis");
+const xAxis = d3.axisBottom(xScale)
+                .tickFormat(d3.format("d") );
 
 svg.append("g")
-    .attr("id","y-axis");
+       .attr("id","x-axis")
+    //   .attr("transform","translate(0," + (h-padding) + ")")
+       .call(xAxis);
 
-
+svg.append("g")
+       .attr("id","y-axis");
+    //   .attr("transform","translate(" + padding + ", 0)")
+    //   .call(yAxis);
 
